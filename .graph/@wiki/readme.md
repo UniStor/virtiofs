583b66aeee703d249f208c06cdb6a239637cfc1a
Dark Star☀🎵 <darkstar@unisocial.net>
2022-10-24 15:25:11 -0500

    try

--- .graph/@wiki/readme.md
+++ .graph/@wiki/readme.md
@@ -7,6 +7,3 @@ guide: https://www.tauceti.blog/posts/qemu-kvm-share-host-directory-with-vm-with
 # Test:
 - status: works!
 I was able to use this guide to share a folder using Virt-Manager gui to configure it!
-
-# virsh
-- test: todo
--- .graph/test/test.sh
+++ .graph/test/test.sh
@@ -6,12 +6,12 @@ in_vm(){
 	manual_mount(){
 		sudo mount -t virtiofs vm1 /home/me/vm1
 	}
-	
+
 	auto_mount_fstab(){
 		echo "vm1 /home/me/vm1 virtiofs rw,_netdev 0 0" | sudo tee -a /etc/fstab
 		sudo mount -a
 	}
-	
+
 	take_ownership(){
 		sudo chown -R me:me vm1
 	}
